96293af1ee209a2a2e1a3c508db733cd3394869d
Dusan Peric <dusan.peric@page.ba>
2025-10-28 19:24:32 +0100

    fast api backend

--- auth/security.py
+++ auth/security.py
@@ -9,7 +9,8 @@ API_KEY_NAME = 'api-key'
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 def get_api_key(api_key: str = Depends(api_key_header)):
-    if api_key != API_KEY:
+    api_keys = API_KEY.split(',')
+    if api_key not in api_keys:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Invalid API Key'
